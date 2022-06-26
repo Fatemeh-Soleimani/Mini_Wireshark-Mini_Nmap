6162f83ac6a4d06b4041c4270aa4993652fdb781
Fatemeh Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2022-06-26 14:28:00 +0430

    fix bug pkt sender

--- pkt-sender.py
+++ pkt-sender.py
@@ -1,20 +1,14 @@
-from email import message
 from socket import *
 from binascii import unhexlify
 
-
 def sendpkt(pkt, interface):
     s = socket(AF_PACKET, SOCK_RAW)
-    s.bind((inter, 0))
-    return s.send(pkt)
-
-
-messageee = input("what is your packet content?")
-messageee = " ".join(messageee[i:i+2] for i in range(0, len(messageee), 2))
-
-inter = input("which interface do you want to use?")
-packet = unhexlify(messageee).replace(" ", "")
-
-# message="ac7ba14f4cfea18"
-
-print("packet sent with %d byte on wlan0 " % sendpkt(packet, inter))
+    s.bind((interface, 0))
+    return s.send(unhexlify(pkt))
+ 
+if __name__ == '__main__':
+    
+    messageee = input("what is your packet content?")
+   
+    inter = input("which interface do you want to use?")
+    print("packet sent with %d byte on wlan0 " % sendpkt(messageee, inter))
