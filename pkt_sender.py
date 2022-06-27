22f7749d218b53fbec599fef1f8a1395f23c8822
Fatemeh Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2022-06-27 21:27:30 +0430

    Update pkt_sender.py

--- pkt_sender.py
+++ pkt_sender.py
@@ -1,16 +1,14 @@
 from socket import *
 from binascii import unhexlify
 
-
 def sendpkt(pkt, interface):
     s = socket(AF_PACKET, SOCK_RAW)
     s.bind((interface, 0))
     return s.send(unhexlify(pkt))
-
-
+ 
 if __name__ == '__main__':
-
+    
     messageee = input("what is your packet content?")
-
+   
     inter = input("which interface do you want to use?")
-    print("packet sent with %d byte on wlan0 " % sendpkt(messageee, inter))
+    print(f"packet sent with %d byte on  {inter}" % sendpkt(messageee, inter))
