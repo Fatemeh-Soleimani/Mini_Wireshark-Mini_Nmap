958ea2f3b5a9735e0aab7648ab4bd2c80e4cc963
sahartvk <80905710+sahartvk@users.noreply.github.com>
2022-06-28 16:18:45 +0430

    Update mininmap_sender.py

--- mininmap_sender.py
+++ mininmap_sender.py
@@ -1,37 +1,25 @@
 from tcp_syn_sender import myfunc
-
-
-
+from socket import *
 
 ip = input("what is the terget ip address?")
 ports = input("which ports do you want to scan?")
 
 # range
 x = ports.split("-")
-
-# change ip in info.txt file
-a_file = open("info.txt", "r")
-list_of_lines = a_file.readlines()
-ip += "\n"
-list_of_lines[0] = ip
-
-a_file = open("info.txt", "w")
-a_file.writelines(list_of_lines)
-a_file.close()
-
-
 temp = ""
 
-for i in range(int(x[0]), int(x[1])):
-    # change port in info.txt file
-    a_file = open("info.txt", "r")
-    list_of_lines = a_file.readlines()
-    temp = str(i)
-    temp += "\n"
-    list_of_lines[1] = temp
 
-    a_file = open("info.txt", "w")
-    a_file.writelines(list_of_lines)
-    a_file.close()
+fd = open("info.txt", 'r')
+lines = fd.readlines()
+dest_mac = lines[6].strip().replace(' ', '')
+src_mac = lines[5].strip().replace(' ', '')
+src_ip = inet_aton(lines[2]).hex()
+dest_ip = inet_aton(ip).hex()
+src_port = "%04x" % int(lines[3])
+interface0 = lines[4].strip()
+fd.close()
+for i in range(int(x[0]), int(x[1])):
+    dest_port = "%04x" % int(i)
     print(f"port {i} : ", end="")
-    myfunc()
+    myfunc(dest_mac, src_mac, dest_ip, src_ip, dest_port, src_port, interface0)
+    
